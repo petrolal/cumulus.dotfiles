dfbbf3c38b82b4bcb538425550b0d843c90abb3e
petrolal <petrolalucas@gmail.com>
2026-08-19 11:19:06 -0300

    feature: knob working

--- bootstrap.sh
+++ bootstrap.sh
@@ -41,7 +41,7 @@ install_system_deps() {
         base-devel git curl wget \
         zsh fontconfig \
         sway waybar kitty wofi swaylock swayidle grim slurp \
-        brightnessctl libpulse swaync mako \
+        brightnessctl libpulse playerctl wireplumber swaync mako \
         chromium firefox \
         neovim \
         docker \
@@ -54,7 +54,7 @@ install_system_deps() {
         build-essential git curl wget \
         zsh fontconfig \
         sway waybar kitty wofi swaylock swayidle grim slurp \
-        brightnessctl pulseaudio-utils sway-notification-center mako \
+        brightnessctl playerctl wireplumber pulseaudio-utils sway-notification-center mako \
         firefox chromium-browser \
         neovim \
         docker.io \
